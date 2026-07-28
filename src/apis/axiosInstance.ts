c7afd029678221fe7f208d7390ba41957ef4e483
kxxnayun <nayun2179@gmail.com>
2026-07-28 17:00:22 +0900

    feat: axios 인스턴스 및 인증 헤더 인터셉터 구현 (#65)

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -2,19 +2,29 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'content-type': 'application/json',
+  },
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const item = localStorage.getItem('accessToken');
-    const token = item ? JSON.parse(item) : null;
+    const accessToken = localStorage.getItem('accessToken');
 
-    if (token) {
+    if (accessToken) {
       config.headers = config.headers || {};
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
 
     return config;
   },
   (error) => Promise.reject(error),
 );
+
+export function setAuthorizationHeader(accessToken: string) {
+  axiosInstance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+}
+
+export function clearAuthorizationHeader() {
+  delete axiosInstance.defaults.headers.common.Authorization;
+}
